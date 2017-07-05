ba7933d88a9993a64ef4a10ec0c22d16c21fa15f
sudhanshu tiwari <sidsid14@gmail.com>
2017-07-05 19:23:04 +0530

    saving chat to a file

--- socketServer.js
+++ socketServer.js
@@ -3,6 +3,8 @@ var morgan = require("morgan");
 var fs = require("fs");
 var express = require("express");
 
+var path = require("path");
+
 var port = process.env.PORT || 5000;
 
 var app = express();
@@ -13,6 +15,22 @@ app.use(morgan("combined"));
 
 var server = http.createServer(app).listen(port);
 
+var jsonData;
+
+app.get("/load",function(req,res){
+
+    
+    var onlyDate = "./chat_file/"+getTodayDate()+".json";
+      fs.exists(onlyDate , function(exists){
+          if(exists){
+              jsonData = fs.readFileSync(onlyDate,'utf-8');
+              var prevMsgs = JSON.parse(jsonData);
+            
+              res.send(prevMsgs);
+          }         
+      });
+    
+});
 
 
 //app.listen(5000);
@@ -39,17 +57,45 @@ wsServer.on("request", function(r){
     var connection = r.accept("echo-protocol", r.origin);
     var color = randomColor();
     var id = count++;        //giving an id to a client
-     connection["color"] = color;
+    connection["color"] = color;
     clients[id] = connection;//storing connection of a client so that we can loop through & contact all clients
     
     console.log( (new Date()) + "Connection accepted [" + id +"]" + connection.remoteAddress);
-    //console.log(clients)
+    
     connection.on("message", function(message){
         var msgString = message.utf8Data;
         var msgArr = msgString.split("@@");
         var dateTime = getDateTime();
-        
+        var onlyDate = "./chat_file/"+getTodayDate()+".json";
       
+        fs.exists(onlyDate , function(exists){
+         var obj = {text:msgArr[0],color:connection.color,sendBy:"",username:msgArr[1],timeStamp:dateTime};
+        
+         if(exists){
+            
+             jsonData = jsonData.substr(0,jsonData.length-1);
+             jsonData += ','+JSON.stringify(obj) + ']';
+             fs.writeFile(onlyDate,jsonData);
+             
+          
+         }else{
+            
+             //Delete rest of the files
+             const directory = "chat_file";
+             
+             fs.readdir("./"+directory, (err, files) => {
+             if(err) throw err;
+            
+                for(var file of files){
+                    fs.unlink(path.join(directory, file));
+                }
+             });
+             
+             fs.writeFileSync(onlyDate,'['+JSON.stringify(obj)+']');
+             jsonData = fs.readFileSync(onlyDate,'utf-8');
+           } 
+        });
+        
         for(var i in clients){
             if(clients[i].remoteAddress == connection.remoteAddress){
                  var msgObject={text:msgArr[0],color:connection.color,sendBy:"Me",username:msgArr[1],timeStamp:dateTime};
@@ -99,5 +145,18 @@ function getDateTime() {
 
 }
 
+function getTodayDate(){
+    var date = new Date();
+    
+    var year = date.getFullYear();
+    
+    var month = date.getMonth();    
+    month = (month < 10 ? "0" : "") + month;
+    
+    var day = date.getDate(); 
+    day = (day < 10 ? "0" : "") + day;
+    
+    return day+month+year;
+}
 
 console.log("server started on port 5000");
